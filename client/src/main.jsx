91902d479e6f012ba5e77877e61a6ee0be3780fc
nepalhomeland <ashokabrother@gmail.com>
2024-07-05 13:08:53 +0545

    27. Complete create listing page UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -13,3 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </PersistGate>
   </Provider>
 );
+
