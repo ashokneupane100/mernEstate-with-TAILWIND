3246d656750a749635e3d46b55ab9d268ce38fe2
nepalhomeland <ashokabrother@gmail.com>
2024-07-20 13:05:20 +0545

    Add Lakhs and Thousand number to words

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom';
 import { MdLocationOn } from 'react-icons/md';
+import { makeLakhs } from '../utils/makeLakhs';
+import { FaBath } from "react-icons/fa";
+import { FaBed } from "react-icons/fa";
+
 
 export default function ListingItem({ listing }) {
   return (
@@ -14,12 +18,12 @@ export default function ListingItem({ listing }) {
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='text-lg font-semibold text-slate-700'>
+          <p className='truncate text-lg font-semibold text-slate-700'>
             {listing.name}
           </p>
           <div className='flex items-center gap-1'>
             <MdLocationOn className='h-4 w-4 text-green-700' />
-            <p className='text-sm text-gray-600 w-full'>
+            <p className='text-sm text-gray-600 truncate w-full'>
               {listing.address}
             </p>
           </div>
@@ -29,17 +33,19 @@ export default function ListingItem({ listing }) {
           <p className='text-slate-500 mt-2 font-semibold '>
             Rs &nbsp;
             {listing.offer
-              ? listing.discountPrice.toLocaleString('en-IN')
-              : listing.regularPrice.toLocaleString('en-IN')}
+              ? makeLakhs(listing.discountPrice)
+              : makeLakhs(listing.regularPrice)}
             {listing.type === 'rent' && ' / month'}
           </p>
-          <div className='text-slate-700 flex gap-4'>
+          <div className='text-slate-700 flex gap-12'>
             <div className='font-bold text-xs'>
+              <FaBed className='h-4 w-4 text-green-700' />
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} beds `
                 : `${listing.bedrooms} bed `}
             </div>
             <div className='font-bold text-xs'>
+              <FaBath className='h-4 w-4 text-green-700' />
               {listing.bathrooms > 1
                 ? `${listing.bathrooms} baths `
                 : `${listing.bathrooms} bath `}
