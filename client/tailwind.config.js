4643f10eb2435bbfe01fb678bc50b46d8b21ceed
nepalhomeland <ashokabrother@gmail.com>
2024-07-22 08:54:41 +0545

    For vercel deployment

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
-  plugins: [
-    // Remove this line
-    // require('@tailwindcss/line-clamp'),
-  ],
-};
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
+});
