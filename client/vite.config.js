fe5c9f7858a0bb908ed38f810b08a175523192ea
nepalhomeland <ashokabrother@gmail.com>
2024-07-22 09:22:45 +0545

    again dependencies changed for vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendors': ['react', 'react-dom'],
-        },
-      },
-      external: ['number-to-words']
-    },
-    chunkSizeWarningLimit: 1000,
+    outDir: 'dist',
   },
 });
