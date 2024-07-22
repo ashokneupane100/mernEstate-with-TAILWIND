e6404edc739e6674a0880adc985020d05ddf30ab
nepalhomeland <ashokabrother@gmail.com>
2024-07-22 09:08:07 +0545

    fix the issues in deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
-  },
-
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendors': ['react', 'react-dom'],
+        },
+      },
+      external: ['number-to-words']
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
